8491f77ad5d661cf81b9a79532bbcc55725b2d61
Ravi Ranjan <raviranjan0631@gmail.com>
2018-03-04 15:13:49 +0530

    second excercise

--- app/src/main/java/com/example/user/sunshine/MainActivity.java
+++ app/src/main/java/com/example/user/sunshine/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.user.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textViews;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViews = (TextView)findViewById(R.id.tv_weather_data);
+        String args[] = {"12 degree cold", "kha hai yeah", "gojo"};
+        for(String arg : args){
+            textViews.append(arg+"\n");
+        }
     }
 }
